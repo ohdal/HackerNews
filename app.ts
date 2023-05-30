c8a9eb6621adbf336c241fcee02f1703ea14b998
윤현정 <yoonhj97@naver.com>
2023-05-30 17:49:43 +0900

    [MOD] app.ts type alias -> interface 변경 & readonly 처리

--- app.ts
+++ app.ts
@@ -1,30 +1,34 @@
-type Store = {
+// Union 타입을 사용해야하면 type alias 를 사용한다
+// 그 이외에는 interface를 더 많이 사용하는 경향이 있다.
+// interface는 사용법을 봤을때 글로써 표현하여(extends) 무슨일을 하는지 더 명확히 표현한다는 장점이 있다.
+
+interface Store {
   currentPage: number;
   feed: NewsFeed[];
 }
 
-type News = {
-  id: Number;
-  time_ago: string;
-  title: string;
-  url : string;
-  user: string;
-  content: string;
+interface News {
+  readonly id: Number;
+  readonly time_ago: string;
+  readonly title: string;
+  readonly url : string;
+  readonly user: string;
+  readonly content: string;
 }
 
-type NewsFeed = News & {
-  comments_count: number;
-  points: number;
+interface NewsFeed extends News {
+  readonly comments_count: number;
+  readonly points: number;
   read?: boolean;
 }
 
-type NewsDetail = News & {
-  comments: NewsComment[];
+interface NewsDetail extends News {
+  readonly comments: NewsComment[];
 }
 
-type NewsComment = News & {
-  comments: NewsComment[];
-  level: number;
+interface NewsComment extends News{
+  readonly comments: NewsComment[];
+  readonly level: number;
 }
 
 const container : HTMLElement | null = document.getElementById("root");
